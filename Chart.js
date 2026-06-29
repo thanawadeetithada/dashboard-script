033b91d78c0121ed8ec95c6b08253832905c798f
Baifern <thanawadee.titha@gmail.com>
2026-06-29 20:14:35 +0700

    update chart

--- Chart.js
+++ Chart.js
@@ -15,24 +15,30 @@ function getChartData() {
       if (!isNaN(d.getTime())) {
         let monthYear = Utilities.formatDate(d, "GMT+07:00", "MMM yyyy");
         if (!monthlyStats[monthYear]) monthlyStats[monthYear] = { in: 0, out: 0 };
-        if (data[i]['รับเข้า (IN)'] !== "") monthlyStats[monthYear].in += data[i]['รับเข้า (IN)'];
-        if (data[i]['จ่ายออก (OUT)'] !== "") monthlyStats[monthYear].out += data[i]['จ่ายออก (OUT)'];
+        if (data[i]['รับเข้า (IN)'] !== "") monthlyStats[monthYear].in += Number(data[i]['รับเข้า (IN)']);
+        if (data[i]['จ่ายออก (OUT)'] !== "") monthlyStats[monthYear].out += Number(data[i]['จ่ายออก (OUT)']);
       }
     }
 
-    let typeName = data[i]['ประเภท/ชื่อ'];
-    if (typeName !== "") {
+    let typeName = data[i]['ชื่อชีท'];
+    if (typeName !== "" && typeName !== undefined) {
       if (!typeStats[typeName]) typeStats[typeName] = 0;
-      if (data[i]['รับเข้า (IN)'] !== "") typeStats[typeName] += data[i]['รับเข้า (IN)'];
+      if (data[i]['รับเข้า (IN)'] !== "") {
+        typeStats[typeName] += Number(data[i]['รับเข้า (IN)']);
+      }
     }
   }
 
-  result.monthly.labels = Object.keys(monthlyStats);
-  result.monthly.inData = Object.keys(monthlyStats).map(k => monthlyStats[k].in);
-  result.monthly.outData = Object.keys(monthlyStats).map(k => monthlyStats[k].out);
-  
-  result.type.labels = Object.keys(typeStats);
-  result.type.data = Object.keys(typeStats).map(k => typeStats[k]);
+  for (let month in monthlyStats) {
+    result.monthly.labels.push(month);
+    result.monthly.inData.push(monthlyStats[month].in);
+    result.monthly.outData.push(monthlyStats[month].out);
+  }
+
+  for (let type in typeStats) {
+    result.type.labels.push(type);
+    result.type.data.push(typeStats[type]);
+  }
 
   return result;
 }
